9fd8fa5a81b73af4ca8a4674232a32cc5fc23e4f
MKletz <michael.kletz.27@gmail.com>
2022-06-16 09:54:29 -0500

    Update build_ee.sh

--- azure_ee/build_ee.sh
+++ azure_ee/build_ee.sh
@@ -4,5 +4,6 @@ pip3 install --upgrade pip --user
 pip3 install ansible-builder
 ansible-builder build --tag azure_snow_ee --container-runtime podman
 
+podman login registry.redhat.io
 podman login docker.io
 podman push azure_snow_ee docker.io/mkletz/azure_snow_ee
