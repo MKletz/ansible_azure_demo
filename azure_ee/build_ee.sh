5408f8a4a5401c04c5e7020788243100392ba0da
mkletz <michael.kletz.27@gmail.com>
2022-06-16 13:48:23 -0500

    adsf

--- azure_ee/build_ee.sh
+++ azure_ee/build_ee.sh
@@ -2,8 +2,8 @@ python3.8 -m venv appenv
 source appenv/bin/activate
 pip3 install --upgrade pip --user
 pip3 install ansible-builder
-ansible-builder build --tag azure_snow_ee --container-runtime podman
+ansible-builder build --tag azure_ee --container-runtime podman
 
 podman login registry.redhat.io
 podman login docker.io
-podman push azure_snow_ee docker.io/mkletz/azure_snow_ee
+podman push azure_ee docker.io/mkletz/azure_ee
